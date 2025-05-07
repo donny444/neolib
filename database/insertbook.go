6f6f197f4bd37a9eb552c75d1ffc4df9a8b19327
donny_444 <tanadornrach@gmail.com>
2025-05-07 22:29:49 +0700

    fix some sql statements for library tables

--- database/insertbook.go
+++ database/insertbook.go
@@ -1,10 +1,15 @@
 package database
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 func InsertBook(ctx context.Context, username string, isbn string, title string, publisher *string, category *string, author *string, pages *string, language *string, publicationYear *string, fileContent []byte) error {
-	_, err := db.ExecContext(ctx, "INSERT INTO ? (isbn, title, publisher, category, author, pages, language, publication_year, file) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-		username,
+	_, err := db.ExecContext(ctx, fmt.Sprintf(
+		"INSERT INTO `%s` (isbn, title, publisher, category, author, pages, language, publication_year, file)"+
+			"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		username),
 		isbn,
 		title,
 		publisher,
--- database/selectbook.go
+++ database/selectbook.go
@@ -3,8 +3,11 @@ package database
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 func SelectBook(ctx context.Context, username string, isbn string) (*sql.Row, error) {
-	return db.QueryRowContext(ctx, "SELECT isbn, title, publisher, category, author, pages, language, publication_year FROM ? WHERE isbn = ?", username, isbn), nil
+	return db.QueryRowContext(ctx, fmt.Sprintf(
+		"SELECT isbn, title, publisher, category, author, pages, language, publication_year FROM `%s`"+
+			"WHERE isbn = ?", username), isbn), nil
 }
