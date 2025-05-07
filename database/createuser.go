6f26e0fccf825d9893239ef5b1d0786bd34adda3
donny_444 <tanadornrach@gmail.com>
2025-05-07 22:24:14 +0700

    fixed schema and file column for library table creation

--- database/createuser.go
+++ database/createuser.go
@@ -22,7 +22,6 @@ func CreateUser(ctx context.Context, username string, email string, password str
 
 	// Create a table for the user
 	_, err = tx.ExecContext(ctx, fmt.Sprintf("CREATE TABLE `%s` ( "+
-		"`uuid` char(36) NOT NULL, "+
 		"`title` varchar(255) NOT NULL, "+
 		"`publisher` varchar(255) DEFAULT NULL, "+
 		"`category` varchar(255) DEFAULT NULL, "+
@@ -31,7 +30,7 @@ func CreateUser(ctx context.Context, username string, email string, password str
 		"`language` varchar(255) DEFAULT NULL, "+
 		"`publication_year` year(4) DEFAULT NULL, "+
 		"`isbn` varchar(20) NOT NULL, "+
-		"`file_content` blob DEFAULT NULL, "+
+		"`file` blob DEFAULT NULL, "+
 		"`created_at` timestamp NOT NULL DEFAULT current_timestamp(), "+
 		"`updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()) "+
 		"ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;", username))
